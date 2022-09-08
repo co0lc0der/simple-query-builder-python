c8d1edd640aa209ce10faa5da8f9b9a8c90efbab
co0lc0der <c0der@ya.ru>
2022-09-08 13:17:30 +0500

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 :copyright: (c) 2022 co0lc0der
 """
 
-version = '0.3.2'
+version = '0.3.3'
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
--- simple_query_builder/__init__.py
+++ simple_query_builder/__init__.py
@@ -7,5 +7,5 @@
 from .querybuilder import *
 
 __author__ = 'co0lc0der'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __email__ = 'c0der@ya.ru'
