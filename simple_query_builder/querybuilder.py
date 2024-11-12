911391174e1f700f33bbc9f5a84a491fc6042945
co0lc0der <c0der@ya.ru>
2024-11-12 16:48:14 +0500

    Move MetaSingleton and DataBase classes into database.py

--- simple_query_builder/querybuilder.py
+++ simple_query_builder/querybuilder.py
@@ -1,42 +1,14 @@
 """
 :authors: co0lc0der
 :license: MIT
-:copyright: (c) 2022-2023 co0lc0der
+:copyright: (c) 2022-2024 co0lc0der
 """
 
 import inspect
-import sqlite3
 import sys
 import traceback
 from typing import Union
-
-
-class MetaSingleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-class DataBase(metaclass=MetaSingleton):
-    db_name = "db.db"
-    conn = None
-    cursor = None
-
-    def connect(self, db_name=""):
-        if db_name != "":
-            self.db_name = db_name
-
-        if self.conn is None:
-            self.conn = sqlite3.connect(self.db_name)
-            self.cursor = self.conn.cursor()
-
-        return self.conn
-
-    def c(self):
-        return self.cursor
+from database import *
 
 
 class QueryBuilder:
