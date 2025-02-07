979a1d73e09c4a1f81710b27bada0f48a857bcb2
co0lc0der <c0der@ya.ru>
2025-02-07 12:51:32 +0500

    improve TABLE methods tests

--- tests/tb_unittest.py
+++ tests/tb_unittest.py
@@ -3,24 +3,49 @@ from simple_query_builder.querybuilder import *
 
 # test run
 # python .\tb_unittest.py -v
-# python -m unittest .\tb_unittest.py
 
 
 class TBTestCase(unittest.TestCase):
     def setUp(self):
         self.qb = QueryBuilder(DataBase(), ":memory:")
 
-    def test_sql_drop_table_no_exists(self):
-        sql = self.qb.drop('temporary', False).get_sql()
-        self.assertEqual(sql, "DROP TABLE `temporary`")
+    def test_drop_table_empty_name(self):
+        result = self.qb.drop('')
 
-    def test_sql_drop_table_exists(self):
-        sql = self.qb.drop('temporary').get_sql()
-        self.assertEqual(sql, "DROP TABLE IF EXISTS `temporary`")
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "Empty table in drop method")
 
-    def test_sql_truncate_table(self):
-        sql = self.qb.truncate('users').get_sql()
-        self.assertEqual(sql, "TRUNCATE TABLE `users`")
+    def test_drop_table_no_exists(self):
+        result = self.qb.drop('temporary', False)
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "DROP TABLE `temporary`")
+        self.assertEqual(result.get_params(), ())
+
+    def test_drop_table_exists(self):
+        result = self.qb.drop('temporary')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "DROP TABLE IF EXISTS `temporary`")
+        self.assertEqual(result.get_params(), ())
+
+    def test_truncate_table_empty_name(self):
+        result = self.qb.truncate('')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "Empty table in truncate method")
+
+    def test_truncate_table(self):
+        result = self.qb.truncate('users')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "TRUNCATE TABLE `users`")
+        self.assertEqual(result.get_params(), ())
 
 
 if __name__ == "__main__":
