0c80efb42679571235851fb974a8aa5216b0a4e2
co0lc0der <c0der@ya.ru>
2025-02-07 11:02:57 +0500

    improve VIEW tests

--- tests/qb_views_unittest.py
+++ tests/qb_views_unittest.py
@@ -9,30 +9,60 @@ class QBViewsTestCase(unittest.TestCase):
     def setUp(self):
         self.qb = QueryBuilder(DataBase(), ":memory:")
 
-    def test_sql_create_view_exists(self):
-        sql = self.qb.select('users').is_null('email').create_view('users_no_email').get_sql()
-        self.assertEqual(sql, "CREATE VIEW IF NOT EXISTS `users_no_email` AS SELECT * FROM `users` WHERE (`email` IS NULL)")
+    def test_create_view_empty_view_name(self):
+        result = self.qb.create_view('')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "Empty view_name in create_view method")
+
+    def test_create_view_no_select(self):
+        result = self.qb.delete('comments').where([['user_id', 10]]).create_view('users_no_email')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "No select found in create_view method")
+
+    def test_create_view_exists(self):
+        result = self.qb.select('users').is_null('email').create_view('users_no_email')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "CREATE VIEW IF NOT EXISTS `users_no_email` AS SELECT * FROM `users` WHERE (`email` IS NULL)")
         self.assertEqual(self.qb.get_params(), ())
 
-    def test_sql_create_view_no_exists(self):
-        sql = self.qb.select('users').is_null('email').create_view('users_no_email', False).get_sql()
-        self.assertEqual(sql, "CREATE VIEW `users_no_email` AS SELECT * FROM `users` WHERE (`email` IS NULL)")
+    def test_create_view_no_exists(self):
+        result = self.qb.select('users').is_null('email').create_view('users_no_email', False)
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "CREATE VIEW `users_no_email` AS SELECT * FROM `users` WHERE (`email` IS NULL)")
         self.assertEqual(self.qb.get_params(), ())
 
-    def test_sql_create_view(self):
-        sql = self.qb.select('users').where([['email', 'is null'], 'or', ['email', '']])\
-               .create_view('users_no_email').get_sql()
-        self.assertEqual(sql, "CREATE VIEW IF NOT EXISTS `users_no_email` AS SELECT * FROM `users` WHERE (`email` IS NULL) OR (`email` = '')")
+    def test_create_view(self):
+        result = self.qb.select('users').where([['email', 'is null'], 'or', ['email', '']])\
+               .create_view('users_no_email')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "CREATE VIEW IF NOT EXISTS `users_no_email` AS SELECT * FROM `users` WHERE (`email` IS NULL) OR (`email` = '')")
         self.assertEqual(self.qb.get_params(), ('', ))
-        # self.qb.go()
 
-    def test_sql_drop_view_no_exists(self):
-        sql = self.qb.drop_view('users_no_email', False).get_sql()
-        self.assertEqual(sql, "DROP VIEW `users_no_email`")
+    def test_drop_view_no_exists(self):
+        result = self.qb.drop_view('users_no_email', False)
 
-    def test_sql_drop_view_exists(self):
-        sql = self.qb.drop_view('users_no_email').get_sql()
-        self.assertEqual(sql, "DROP VIEW IF EXISTS `users_no_email`")
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "DROP VIEW `users_no_email`")
+        self.assertEqual(self.qb.get_params(), ())
+
+    def test_drop_view_exists(self):
+        result = self.qb.drop_view('users_no_email')
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "DROP VIEW IF EXISTS `users_no_email`")
+        self.assertEqual(self.qb.get_params(), ())
 
 
 if __name__ == "__main__":
