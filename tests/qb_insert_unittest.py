220826e9f387c5803b1a68cf376010f2e5f00b72
co0lc0der <c0der@ya.ru>
2025-02-07 13:31:45 +0500

    improve INSERT tests

--- tests/qb_insert_unittest.py
+++ tests/qb_insert_unittest.py
@@ -9,19 +9,69 @@ class QBInsertTestCase(unittest.TestCase):
     def setUp(self):
         self.qb = QueryBuilder(DataBase(), ":memory:")
 
-    def test_sql_insert(self):
-        sql = self.qb.insert('groups', {'name': 'Moderator', 'permissions': 'moderator'}).get_sql()
-        self.assertEqual(sql, "INSERT INTO `groups` (`name`, `permissions`) VALUES ('Moderator','moderator')")
-        self.assertEqual(self.qb.get_params(), ('Moderator', 'moderator'))
+    def test_insert_empty_table(self):
+        result = self.qb.insert('', {'param': 'new_value'})
 
-    def test_sql_insert_multiple(self):
-        sql = self.qb.insert('groups', [
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "Empty table or fields in insert method")
+
+    def test_insert_empty_fields(self):
+        result = self.qb.insert('params', {})
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "Empty table or fields in insert method")
+
+    def test_insert_empty_table_and_fields(self):
+        result = self.qb.insert('', {})
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), True)
+        self.assertEqual(result.get_error_message(), "Empty table or fields in insert method")
+
+    def test_insert_table_as_str(self):
+        result = self.qb.insert('groups', {'name': 'Moderator', 'permissions': 'moderator'})
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "INSERT INTO `groups` (`name`, `permissions`) VALUES ('Moderator','moderator')")
+        self.assertEqual(result.get_params(), ('Moderator', 'moderator'))
+
+    def test_insert_table_as_dict(self):
+        result = self.qb.insert({'g': 'groups'}, {'name': 'Moderator', 'permissions': 'moderator'})
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "INSERT INTO `groups` AS `g` (`name`, `permissions`) VALUES ('Moderator','moderator')")
+        self.assertEqual(result.get_params(), ('Moderator', 'moderator'))
+
+    def test_insert_multiple_as_str(self):
+        result = self.qb.insert('groups', [
                             ['name', 'role'],
                             ['Moderator', 'moderator'], ['Moderator2', 'moderator'],
                             ['User', 'user'], ['User2', 'user']
-                        ]).get_sql()
-        self.assertEqual(sql, "INSERT INTO `groups` (`name`, `role`) VALUES ('Moderator','moderator'),('Moderator2','moderator'),('User','user'),('User2','user')")
-        self.assertEqual(self.qb.get_params(), ('Moderator', 'moderator', 'Moderator2', 'moderator', 'User', 'user', 'User2', 'user'))
+                        ])
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "INSERT INTO `groups` (`name`, `role`) VALUES ('Moderator','moderator'),('Moderator2','moderator'),('User','user'),('User2','user')")
+        self.assertEqual(result.get_params(), ('Moderator', 'moderator', 'Moderator2', 'moderator', 'User', 'user', 'User2', 'user'))
+
+    def test_insert_multiple_as_dict(self):
+        result = self.qb.insert({'g': 'groups'}, [
+                            ['name', 'role'],
+                            ['Moderator', 'moderator'], ['Moderator2', 'moderator'],
+                            ['User', 'user'], ['User2', 'user']
+                        ])
+
+        self.assertEqual(result, self.qb)
+        self.assertEqual(result.has_error(), False)
+        self.assertEqual(result.get_sql(), "INSERT INTO `groups` AS `g` (`name`, `role`) VALUES ('Moderator','moderator'),('Moderator2','moderator'),('User','user'),('User2','user')")
+        self.assertEqual(result.get_params(), ('Moderator', 'moderator', 'Moderator2', 'moderator', 'User', 'user', 'User2', 'user'))
+
+    def tearDown(self):
+        pass
 
 
 if __name__ == "__main__":
